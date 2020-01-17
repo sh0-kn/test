ebc1d3ebb604f885837c1882de659a2c2da02811
sh0-kn <u.luv.k1113@gmail.com>
2020-01-17 18:01:48 +0900

    コメントを解除

--- test.js
+++ test.js
@@ -5,21 +5,19 @@ function checkUserName(userName) {
     data: userName,
     scriptCharset: 'utf-8'
   })
-//  .then((result) => {
-//  	console.log(result);
-//    if (result) {  // 重複あり
-//    	$('button#confirm').prop('disabled', true);
-//    	$("#duplicatedUserName").dialog("open");
-//    	//$('button#checkUserName').prop('class', '');
-//    	$('span#checkOK').prop('class', 'hidden');
-//    } else {      // 重複なし
-//    	$('button#confirm').prop('disabled', false);
-//    	//$('button#checkUserName').prop('class', 'hidden');
-//    	$('span#checkOK').prop('class', 'info');
-//    }
-//  }, () => {
-//    console.error('Error: ajax connection failed.');
-//  })
-;
-
+  .then((result) => {
+  	console.log(result);
+    if (result) {  // 重複あり
+    	$('button#confirm').prop('disabled', true);
+    	$("#duplicatedUserName").dialog("open");
+    	//$('button#checkUserName').prop('class', '');
+    	$('span#checkOK').prop('class', 'hidden');
+    } else {      // 重複なし
+    	$('button#confirm').prop('disabled', false);
+    	//$('button#checkUserName').prop('class', 'hidden');
+    	$('span#checkOK').prop('class', 'info');
+    }
+  }, () => {
+    console.error('Error: ajax connection failed.');
+  });
 }
