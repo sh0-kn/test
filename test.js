f08988ee04c786a9e7098f935003e029811a97ae
sh0-kn <u.luv.k1113@gmail.com>
2020-01-17 17:54:36 +0900

    test

---
+++ test.js
@@ -0,0 +1,23 @@
+function checkUserName(userName) {
+	$.ajax({
+    type: 'POST',
+    url: '/sampleweb/cart/delete',
+    data: userName,
+    scriptCharset: 'utf-8'
+  })
+  .then((result) => {
+  	console.log(result);
+    if (result) {  // d•¡‚ ‚è
+    	$('button#confirm').prop('disabled', true);
+    	$("#duplicatedUserName").dialog("open");
+    	//$('button#checkUserName').prop('class', '');
+    	$('span#checkOK').prop('class', 'hidden');
+    } else {      // d•¡‚È‚µ
+    	$('button#confirm').prop('disabled', false);
+    	//$('button#checkUserName').prop('class', 'hidden');
+    	$('span#checkOK').prop('class', 'info');
+    }
+  }, () => {
+    console.error('Error: ajax connection failed.');
+  });
+}
